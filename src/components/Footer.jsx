4cf4d23e37c5c9437412a855f2f5bd2418cd17b7
Wealthometer <wealthhajoh87@gmail.com>
2026-05-03 00:02:02 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -66,3 +66,4 @@ const Footer = () => {
           </a>
         </div>
         <a href='' className='flex items-center gap-2 underline'>
+          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
