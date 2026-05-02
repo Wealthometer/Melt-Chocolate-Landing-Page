c968312d9639aae04169e84a94d0fd7224450a0b
Wealthometer <wealthhajoh87@gmail.com>
2026-05-03 00:29:44 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -42,4 +42,5 @@ const Header = () => {
             <rect width="32" height="32" rx="16" transform="matrix(-1 0 0 1 32 0)" fill="#FF6B57"/>
             <path d="M23 16H9M23 16L17 22M23 16L17 10" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
+        </a>
       </header>
