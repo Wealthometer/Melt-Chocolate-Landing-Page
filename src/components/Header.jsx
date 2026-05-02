8751f510da1b1b872e995fc9806b3df1effe6631
Wealthometer <wealthhajoh87@gmail.com>
2026-05-03 00:10:22 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,3 +18,4 @@ const Header = () => {
   return (
     <>
       <header className='sticky top-0 z-20 flex items-stretch justify-between gap-5 max-w-7xl m-auto p-6'>
+        <div className="flex items-center justify-between bg-white rounded-xl w-full px-4 py-3">
