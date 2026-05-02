868bdb9089d684e7ee94677a20942783c860a65c
Wealthometer <wealthhajoh87@gmail.com>
2026-05-03 00:10:26 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,3 +20,4 @@ const Header = () => {
       <header className='sticky top-0 z-20 flex items-stretch justify-between gap-5 max-w-7xl m-auto p-6'>
         <div className="flex items-center justify-between bg-white rounded-xl w-full px-4 py-3">
           <img src={Logo} alt="Melt Chocolate" className='max-w-36' />
+          <nav>
