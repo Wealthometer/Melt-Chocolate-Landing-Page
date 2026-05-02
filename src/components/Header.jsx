1a6807e80e1f4203402e1e5e5ef888e0cef7a2dd
Wealthometer <wealthhajoh87@gmail.com>
2026-05-03 00:28:41 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,3 +39,4 @@ const Header = () => {
         </div>
         <a href="" className='flex items-center justify-center bg-white rounded-xl text-nowrap px-4 py-3 font-medium gap-3'>Explore Flavors 
           <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="32" height="32" rx="16" transform="matrix(-1 0 0 1 32 0)" fill="#FF6B57"/>
