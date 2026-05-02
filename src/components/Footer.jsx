97dec6bb28c2f2bb073ae40cbc7f981d37c10ffe
Wealthometer <wealthhajoh87@gmail.com>
2026-05-03 00:00:47 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -40,3 +40,4 @@ const Footer = () => {
         <div className='col-span-2'>
           <h3 className='text-xl font-bold mb-4'>Experience</h3>
           <ul className='flex flex-col gap-3'>
+            <li><a href="" className='hover:underline'>Taste Journey</a></li>
