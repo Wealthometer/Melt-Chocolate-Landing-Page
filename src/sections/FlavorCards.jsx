4b740b918a0186cbf970eab0c060d2d5e7c419de
Wealthometer <wealthhajoh87@gmail.com>
2026-05-04 15:21:54 +0100

    Update FlavorCards.jsx

--- src/sections/FlavorCards.jsx
+++ src/sections/FlavorCards.jsx
@@ -4,3 +4,4 @@ const FlavorCards = ({ setActiveFlavor }) => {
   return (
     <>
       <div className='grid grid-cols-4 gap-5 items-end pb-30 h-screen'>
+        <div className='relative group card caramel-card bg-orange w-full h-[40vh] rounded-xl origin-top     transform transition-transform duration-500 ease-out hover:scale-y-[1.08] flex flex-col items-center justify-end overflow-hidden pb-8'
