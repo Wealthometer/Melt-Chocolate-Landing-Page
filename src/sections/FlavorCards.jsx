ed759f5eb84eda0dcb50f564c8ab113d968822f5
Wealthometer <wealthhajoh87@gmail.com>
2026-05-04 15:23:05 +0100

    Update FlavorCards.jsx

--- src/sections/FlavorCards.jsx
+++ src/sections/FlavorCards.jsx
@@ -20,3 +20,4 @@ const FlavorCards = ({ setActiveFlavor }) => {
                 translate-y-6 group-hover:opacity-100 
                 group-hover:translate-y-0
                 transition-all duration-500 ease-out">
+            Bold cocoa with a<br />deep finish.
