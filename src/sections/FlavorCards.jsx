5b2e33ae35f174cf2f4df6d65d20ebbf53a9ad4c
Wealthometer <wealthhajoh87@gmail.com>
2026-05-04 15:22:18 +0100

    Update FlavorCards.jsx

--- src/sections/FlavorCards.jsx
+++ src/sections/FlavorCards.jsx
@@ -12,5 +12,6 @@ const FlavorCards = ({ setActiveFlavor }) => {
                 group-hover:translate-y-0
                 transition-all duration-500 ease-out">
             Sweet crunch,<br />slow melt.
+          </p>
         </div>
         <div className='relative group card cocoa-card bg-blue w-full h-[40vh] rounded-xl origin-top     transform transition-transform duration-500 ease-out hover:scale-y-[1.08] flex flex-col items-center justify-end overflow-hidden pb-8' onMouseEnter={() => setActiveFlavor('cocoa')}
