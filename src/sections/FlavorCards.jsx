04dda8ce1bc30a1bc168b55d9830fd8a1ff1b373
Wealthometer <wealthhajoh87@gmail.com>
2026-05-04 15:24:02 +0100

    Update FlavorCards.jsx

--- src/sections/FlavorCards.jsx
+++ src/sections/FlavorCards.jsx
@@ -39,3 +39,4 @@ const FlavorCards = ({ setActiveFlavor }) => {
                 group-hover:translate-y-0
                 transition-all duration-500 ease-out">
             Nutty comfort, <br />perfectly smooth.
+          </p>
