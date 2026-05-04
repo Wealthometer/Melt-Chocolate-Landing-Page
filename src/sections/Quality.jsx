9831be006e0a55d074b64105aa5868b3963e51f0
Wealthometer <wealthhajoh87@gmail.com>
2026-05-04 22:49:18 +0100

    Update Quality.jsx

--- src/sections/Quality.jsx
+++ src/sections/Quality.jsx
@@ -79,3 +79,4 @@ const Quality = () => {
 
           <div
             ref={(el) => (cardsRef.current[2] = el)}
+            className="card bg-green text-white rounded-3xl max-w-115 w-full p-10 min-h-112 flex flex-col justify-between absolute top-32 right-0"
