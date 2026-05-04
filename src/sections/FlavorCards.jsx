5c53e2a324757e66c89c5a97f61b6f307e4c8c71
Wealthometer <wealthhajoh87@gmail.com>
2026-05-04 15:22:14 +0100

    Update FlavorCards.jsx

--- src/sections/FlavorCards.jsx
+++ src/sections/FlavorCards.jsx
@@ -11,3 +11,5 @@ const FlavorCards = ({ setActiveFlavor }) => {
                 translate-y-6 group-hover:opacity-100 
                 group-hover:translate-y-0
                 transition-all duration-500 ease-out">
+            Sweet crunch,<br />slow melt.
+        </div>
