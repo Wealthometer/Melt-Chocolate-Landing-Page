664b5cf2b0970e3f88b9183a58231e71bdb9dac3
Wealthometer <wealthhajoh87@gmail.com>
2026-05-04 15:29:42 +0100

    Update HeroChocolates.jsx

--- src/sections/HeroChocolates.jsx
+++ src/sections/HeroChocolates.jsx
@@ -1,3 +1,4 @@
 import React from 'react'
 import Caramel from '../assets/images/crispy-caramel.png'
 import Cocoa from '../assets/images/dark-cocoa.png'
+import Orange from '../assets/images/orange-zest-milk.png'
